50663365bc5132971c37a381c5916be4513da3fc
David Eastman <davidjeastman@gmail.com>
2017-06-27 12:41:37 -0400

    Fix rotation bug with savedInstanceState

--- app/src/main/java/com/davidjeastman/hardcoretrivia/StageEndFragment.java
+++ app/src/main/java/com/davidjeastman/hardcoretrivia/StageEndFragment.java
@@ -21,34 +21,25 @@ public class StageEndFragment extends Fragment {
 
     private static final String TAG = "StageEndFragment";
     private static final String ARG_QUESTION_LIST_ID = "question_list_id";
-    int mStage;
+    private static final String KEY_UPDATED = "updated";
 
     private List<Question> mQuestions;
 
+    private int mStage;
     private int mProfilePoints;
-
     private int mNumCorrect;
     private int mStagePoints;
     private int mTimeBonusPoints;
 
+    private boolean mIsUpdated = false;
+
     private View.OnClickListener continueTryAgainButtonClick = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Button thisButton = (Button) v;
-
-            if (thisButton.getText().equals(getString(R.string.continue_button))) {
-                StageLoadFragment nextFrag= StageLoadFragment.newInstance();
-                getFragmentManager().beginTransaction()
-                        .replace(R.id.stage_container, nextFrag,TAG)
-                        .commit();
-            } else {
-                StageLoadFragment nextFrag= StageLoadFragment.newInstance();
-                getFragmentManager().beginTransaction()
-                        .replace(R.id.stage_container, nextFrag,TAG)
-                        .commit();
-            }
-
-
+            StageLoadFragment nextFrag = StageLoadFragment.newInstance();
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.stage_container, nextFrag, TAG)
+                    .commit();
         }
     };
 
@@ -66,6 +57,10 @@ public class StageEndFragment extends Fragment {
         setRetainInstance(true);
 
         Profile profile = ProfileManager.get(getActivity()).getProfile();
+        if (savedInstanceState != null) {
+            mIsUpdated = savedInstanceState.getBoolean(KEY_UPDATED);
+        }
+
         mProfilePoints = profile.getPoints();
         mStage = profile.getStage();
 
@@ -87,10 +82,10 @@ public class StageEndFragment extends Fragment {
         Button mStageEndContinueTryAgainButton = v.findViewById(R.id.stage_end_continue_try_again_button);
 
         mQuestions = (ArrayList) getArguments().getSerializable(ARG_QUESTION_LIST_ID);
-        calculateScore();
 
-        double threshold = mQuestions.size() * .7;
-        boolean isStagePassed = mNumCorrect > threshold;
+        boolean isStagePassed = calculateScore();
+
+        if (!mIsUpdated) updateProfile(isStagePassed);
 
         if (isStagePassed) {
             v.getRootView()
@@ -102,12 +97,6 @@ public class StageEndFragment extends Fragment {
                             + " " + getString(R.string.completed));
             mStageEndContinueTryAgainButton
                     .setText(R.string.continue_button);
-
-            ProfileManager pm = ProfileManager.get(getActivity());
-            Profile profile = pm.getProfile();
-            profile.increaseSkill();
-            profile.increaseStage();
-            pm.updateProfile(profile);
 
         } else {
             v.getRootView()
@@ -123,17 +112,14 @@ public class StageEndFragment extends Fragment {
             mStageEndContinueTryAgainButton
                     .setText(R.string.try_again_button);
 
-            mStagePoints = 0;
-            mTimeBonusPoints = 0;
 
-            ProfileManager.get(getActivity()).getProfile().reduceSkill();
         }
 
         mStageEndCorrectAnswersTextView
-                .setText(String.valueOf(mNumCorrect)+ " / "
+                .setText(String.valueOf(mNumCorrect) + " / "
                         + String.valueOf(mQuestions.size()) + " " + getString(R.string.correct_answers));
         mStageEndPointsFractionTextView
-                .setText(String.valueOf(mProfilePoints + mStagePoints) + " / " + 4000);
+                .setText(String.valueOf(mProfilePoints) + " / " + 4000);
         mStageEndPointsAbbrevTextView
                 .setText(String.valueOf(mStagePoints));
         mStageEndTimeBonusPtsAbbrevTextView
@@ -144,8 +130,14 @@ public class StageEndFragment extends Fragment {
         return v;
     }
 
-    private void calculateScore() {
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putBoolean(KEY_UPDATED, mIsUpdated);
+    }
 
+    private boolean calculateScore() {
         mNumCorrect = 0;
         mStagePoints = 0;
         for (Question q : mQuestions) {
@@ -154,6 +146,32 @@ public class StageEndFragment extends Fragment {
                 mNumCorrect++;
                 mStagePoints = mStagePoints + q.getDifficulty() * 100;
             }
+        }
+
+        double threshold = mQuestions.size() * .7;
+        boolean isStagePassed = mNumCorrect > threshold;
+
+
+        return isStagePassed;
+    }
+
+    private void updateProfile(boolean isStagePassed) {
+        mIsUpdated = true;
+        if (isStagePassed) {
+            ProfileManager pm = ProfileManager.get(getActivity());
+            Profile profile = pm.getProfile();
+            profile.increaseSkill();
+            profile.increaseStage();
+
+            mProfilePoints += mStagePoints;
+            profile.setPoints(mProfilePoints);
+
+            pm.updateProfile(profile);
+        } else {
+            mStagePoints = 0;
+            mTimeBonusPoints = 0;
+
+            ProfileManager.get(getActivity()).getProfile().reduceSkill();
         }
     }
 }
